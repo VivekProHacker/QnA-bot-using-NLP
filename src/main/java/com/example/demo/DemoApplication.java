fca9c8b3cbbdeaf0d5320c5e2884157c48ad2448
Vivek Kumar Singh <vsviveksingh30@gmail.com>
2018-06-06 18:35:02 +0530

    fixed dependencies

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration( exclude = {HibernateJpaAutoConfiguration.class,
+		JpaRepositoriesAutoConfiguration.class})
 public class DemoApplication {
 
 	public static void main(String[] args) {
